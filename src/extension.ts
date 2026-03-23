e9ba206509969b306872c7e332f483bc87a0d7e5
ts-achieve <ts.achieve@yahoo.com>
2026-03-22 18:02:46 -0700

    achieve

--- src/extension.ts
+++ src/extension.ts
@@ -15,83 +15,89 @@ export function activate(context: vscode.ExtensionContext) {
     context.globalState.get("achieves") ?? "deleted gobal state",
   );
 
-  withConfig((config, tracer, disposables) => {
-    const achievedProvider = new AchievedProvider(config, tracer, context);
+  context.subscriptions.push(
+    ...withConfig((config, tracer, disposables) => {
+      const achievedProvider = new AchievedProvider(config, tracer, context);
 
-    const summaryProvider = new SummaryProvider(
-      config,
-      tracer,
-      achievedProvider.achieveMap,
-    );
-    disposables.push(
-      vscode.window.registerTreeDataProvider(
-        names.views.summary,
-        summaryProvider,
-      ),
-    );
+      const summaryProvider = new SummaryProvider(
+        config,
+        tracer,
+        achievedProvider.achieveMap,
+      );
+      disposables.push(
+        vscode.window.registerTreeDataProvider(
+          names.views.summary,
+          summaryProvider,
+        ),
+      );
 
-    disposables.push(
-      vscode.window.registerTreeDataProvider(
-        names.views.list,
-        achievedProvider,
-      ),
-    );
+      disposables.push(
+        vscode.window.registerTreeDataProvider(
+          names.views.list,
+          achievedProvider,
+        ),
+      );
 
-    const decorationProvider = new DecorationProvider(tracer);
-    disposables.push(
-      vscode.window.registerFileDecorationProvider(decorationProvider),
-    );
+      const decorationProvider = new DecorationProvider(tracer);
+      disposables.push(
+        vscode.window.registerFileDecorationProvider(decorationProvider),
+      );
 
-    const speedrunProvider = new SpeedrunProvider(config, tracer);
-    disposables.push(
-      vscode.window.registerTreeDataProvider(
-        names.views.speedrun,
-        speedrunProvider,
-      ),
-    );
+      const speedrunProvider = new SpeedrunProvider(config, tracer);
+      disposables.push(
+        vscode.window.registerTreeDataProvider(
+          names.views.speedrun,
+          speedrunProvider,
+        ),
+      );
 
-    vscode.workspace.onDidChangeTextDocument((event) => {
-      const diagnostics = vscode.languages.getDiagnostics(event.document.uri);
-      if (diagnostics.length > 0) {
-        for (let i = 0; i < diagnostics.length; i++) {
-          const diagnostic = diagnostics[i]!;
+      vscode.workspace.onDidChangeTextDocument((event) => {
+        const diagnostics = vscode.languages.getDiagnostics(event.document.uri);
+        if (diagnostics.length > 0) {
+          for (let i = 0; i < diagnostics.length; i++) {
+            const diagnostic = diagnostics[i]!;
 
-          if (typeof diagnostic.code === "number") {
-            const achieve = achievedProvider.achieveMap.get(diagnostic.code);
+            if (typeof diagnostic.code === "number") {
+              const achieve = achievedProvider.achieveMap.get(diagnostic.code);
 
-            if (achieve) {
-              if (achieve.isUnlocked) {
-                achieve.encounter(event, diagnostic);
+              if (achieve) {
+                if (achieve.isUnlocked) {
+                  achieve.encounter(event, diagnostic);
 
-                if (getConfigSection(names.config.notifyRepeatedAchievements)) {
+                  if (
+                    getConfigSection(names.config.notifyRepeatedAchievements)
+                  ) {
+                    vscode.window.showInformationMessage(
+                      `Achievement found again!\n${diagnostic.code}: ${diagnostic.message}`,
+                    );
+                  }
+                } else {
+                  achieve.unlock(event, diagnostic);
                   vscode.window.showInformationMessage(
-                    `Achievement found again!\n${diagnostic.code}: ${diagnostic.message}`,
+                    `Achievement unlocked!\n${diagnostic.code}: ${diagnostic.message}`,
                   );
+                  achievedProvider.refresh();
+                  summaryProvider.refresh(achievedProvider.achieveMap);
                 }
-              } else {
-                achieve.unlock(event, diagnostic);
-                vscode.window.showInformationMessage(
-                  `Achievement unlocked!\n${diagnostic.code}: ${diagnostic.message}`,
-                );
-                achievedProvider.refresh();
-                summaryProvider.refresh(achievedProvider.achieveMap);
               }
             }
           }
+
+          setAchieveMap(context, tracer, achievedProvider.achieveMap);
         }
+      });
 
-        setAchieveMap(context, tracer, achievedProvider.achieveMap);
-      }
-    });
+      setAchieveMap(context, tracer, achievedProvider.achieveMap);
 
-    setAchieveMap(context, tracer, achievedProvider.achieveMap);
-
-    return {
-      context,
-      achievedProvider,
-      summaryProvider,
-      speedrunProvider,
-      decorationProvider,
-    };
-  });
+      return {
+        context,
+        achievedProvider,
+        summaryProvider,
+        speedrunProvider,
+        decorationProvider,
+      };
+    }),
+  );
 }
+
+export function deactivate() {}
