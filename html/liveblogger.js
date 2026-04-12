60ac4f53b2ecb9598e78cbaf86f0e719e98b1ca7
ts-achieve <ts.achieve@yahoo.com>
2026-04-11 20:45:01 -0700

    achieve liveblog

---
+++ html/liveblogger.js
@@ -0,0 +1,114 @@
+window.onload = () => {
+  try {
+    const everything = {
+      state: {
+        combo: 0,
+      },
+      dom: {
+        frame: document.getElementById("frame"),
+        world: document.getElementById("frame"),
+        comboBg: document.getElementById("combo-bg"),
+        comboFg: document.getElementById("combo-fg"),
+      },
+    };
+
+    window.addEventListener("message", (event) => {
+      try {
+        const message = event.data;
+        switch (message.type) {
+          case "star": {
+            makeH1(message.value[0], everything.dom.world);
+            everything.state.combo++;
+            break;
+          }
+          default: {
+            log("implement me");
+          }
+        }
+      } catch (error) {
+        log(error, error.stack);
+      }
+    });
+
+    requestAnimationFrame((now) => tick(now, 0, everything));
+  } catch (error) {
+    log(error, error.stack);
+  }
+};
+
+const tick = (now, last, everything) => {
+  try {
+    everything.state.combo = clamp(0, 100)(everything.state.combo - 0.1);
+
+    everything.dom.comboFg.style.width =
+      unpx(getComputedStyle(everything.dom.comboBg).width) *
+        (everything.state.combo / 100) +
+      "px";
+
+    document.documentElement.style.setProperty(
+      "--combo-width",
+      `${everything.state.combo}%`,
+    );
+    requestAnimationFrame((now) => tick(now, last, everything));
+  } catch (error) {
+    log(error, error.stack);
+  }
+};
+
+const makeH1 = (code, world) => {
+  try {
+    const h1 = document.createElement("h1");
+    h1.innerHTML = code.toString();
+    world.appendChild(h1);
+
+    const worldStyle = getComputedStyle(world);
+    const worldWidth = unpx(worldStyle.width);
+    const worldHeight = unpx(worldStyle.height);
+
+    const h1Style = getComputedStyle(h1);
+    const h1Width = unpx(h1Style.width);
+    const h1Height = unpx(h1Style.height);
+
+    h1.style.left = `${(worldWidth - h1Width) * Math.random()}px`;
+    h1.style.top = `${(worldHeight - h1Height) * Math.random()}px`;
+    h1.style.rotate = `${30 * Math.random() - 15}deg`;
+
+    h1.addEventListener("animationend", () => {
+      if (h1.classList.contains("entering")) {
+        h1.classList.remove("entering");
+        h1.classList.add("exiting");
+      } else if (h1.classList.contains("exiting")) {
+        h1.classList.remove("exiting");
+        world.removeChild(h1);
+      }
+    });
+
+    h1.classList.add("entering");
+    return h1;
+  } catch (error) {
+    log(error, error.stack);
+  }
+};
+
+const log = (...args) => {
+  try {
+    document.getElementById("log").innerHTML =
+      `<p>${new Date().toLocaleTimeString()}: ${args.join(" ")}</p>` +
+      document.getElementById("log").innerHTML;
+  } catch (error) {
+    log(error, error.stack);
+    console.log(error, error.stack);
+  }
+};
+
+const mod = (n, d) => ((n % d) + d) % d;
+
+const stringify = (x) =>
+  ["string", "number"].includes(typeof x) ? x : JSON.stringify(x, null, 2);
+
+const maybeGet = (x, key, fallback = undefined) =>
+  typeof x === "object" && !!x && key in x ? x[key] : fallback;
+
+const clamp = (min, max) => (n) => Math.min(max, Math.max(min, n));
+
+const unpx = (x) => parseFloat(x.slice(-2) === "px" ? x.slice(0, -2) : x);
--- src/provider/liveblogger.ts
+++ src/provider/liveblogger.ts
@@ -4,10 +4,11 @@ import { names } from "../util/const";
 import { consoleErr } from "../util/console";
 import { UnlockedStar } from "../star/star";
 import { WebviewProviderBase } from "./provider";
+import { StarKind } from "../star/taxonomy";
 
 type LivebloggerMessage = {
-  type: "";
-  value: "";
+  type: "star";
+  value: [number, StarKind];
 };
 
 export class Liveblogger extends WebviewProviderBase {
@@ -17,8 +18,13 @@ export class Liveblogger extends WebviewProviderBase {
     super(_extensionUri, "liveblogger");
   }
 
-  update(_star: UnlockedStar) {
-    throw new Error("implement me");
+  update(star: UnlockedStar) {
+    if (this._view) {
+      this._view.webview.postMessage({
+        type: "star",
+        value: [star.code, star.kind],
+      } satisfies LivebloggerMessage);
+    }
   }
 
   _onDidReceiveMessage(data: LivebloggerMessage) {
